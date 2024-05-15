91a9f31a86119ac8e1d32ae5a5be52e8a19de833
Connor812 <connorsavoy812@gmail.com>
2024-05-15 14:19:28 -0400

    now that i have the categories too, i needed to change the way the objects organiztion is done

--- src/components/itemCardModal.js
+++ src/components/itemCardModal.js
@@ -6,11 +6,25 @@ function ItemCardModal({ soap, related_objects, index }) {
     const soapIndex = `soap_${id}`;
     const name = soap.item_data.name;
     const description = soap.item_data.description;
-    const priceInCents = soap.item_data.variations[0].item_variation_data.price_money.amount;
-    const price = parseFloat(soap.item_data.variations[0].item_variation_data.price_money.amount / 100).toFixed(2);
-    const imageId = soap.item_data.image_ids[0];
-    const imageObject = related_objects.find(obj => obj.id === imageId);
-    const imageUrl = imageObject.image_data.url;
+
+    let priceInCents;
+    let price;
+    if (soap.item_data.variations[0].item_variation_data.price_money) {
+        priceInCents = soap.item_data.variations[0].item_variation_data.price_money.amount;
+        price = parseFloat(priceInCents / 100).toFixed(2);
+    } else {
+        priceInCents = 0;
+        price = parseFloat(priceInCents / 100).toFixed(2);
+    }
+
+    let imageUrl;
+    if (soap.item_data.image_ids) {
+        const imageId = soap.item_data.image_ids[0];
+        const imageObject = related_objects.find(obj => obj.id === imageId);
+        imageUrl = imageObject.image_data.url;
+    } else {
+        imageUrl = "http://northshoresoapworks.com/images/replacement-image.png";
+    }
 
     return (
         <div key={index} className="modal fade" id={`${soapIndex}`} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -43,12 +57,19 @@ function ItemCardModal({ soap, related_objects, index }) {
                             <br />
                             <div>
                                 Add <input type="number" min="1" placeholder="1" className={`${soapIndex}Input quantity`} /> To Cart
-                                <button id={`soap-buy-btn-${soapIndex}`} className="buy-button" onClick={() => addItemToCart(id, name, soapIndex, priceInCents, imageUrl, description)}>
-                                    <svg width="25" height="25" viewBox="0 0 16 16">
-                                        <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z" />
-                                        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
-                                    </svg>
-                                </button>
+
+                                {priceInCents === 0 ? (
+                                    <button className="buy-button" disabled>
+                                        Out of Stock
+                                    </button>
+                                ) : (
+                                    <button id={`soap-buy-btn-${soapIndex}`} className="buy-button" onClick={() => addItemToCart(id, name, soapIndex, priceInCents, imageUrl, description)}>
+                                        <svg width="25" height="25" viewBox="0 0 16 16">
+                                            <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z" />
+                                            <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                                        </svg>
+                                    </button>
+                                )}
                             </div>
                         </div>
                     </div>
